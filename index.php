5b2d43c2e4b62c5d96921096e2b7693d40103b3e
ricsoh <ricsoh@gmail.com>
2020-11-03 10:12:31 +0800

    Our 1st group project!

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html> 
+<html>
+  
+<script></script>  
+  
+<style></style>
+  
+<body> 
+  
+  <h1>
+  Hello team memeber
+  </h1>
+  
+  <ul>
+    <li>member 1, your name</li>
+    <li>member 2, your name</li>
+    <li>member 3, your name</li>
+    <li>member 4, your name</li>
+  </ul>
+  
+  <hr>
+  
+  </body> 
+</html>
